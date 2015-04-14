8715d3c3a268b5e1007c6c4268406b021867f143
jagan359 <jagan26@gmail.com>
2015-04-14 14:09:36 +0100

    charlies code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+  
+  @t1= Time.now.strftime("%Y-%m-%d %H:%M:%S.%L");
+  @ta=@t1.to_time
+  	puts '**************************************************************************************************';
+  	 n=999;
+  	10000000.times do |n|
+  n=n*n;
+end
+
+@t2= Time.now.strftime("%Y-%m-%d %H:%M:%S.%L");
+@tb=@t2.to_time
+@t3=(@tb-@ta).to_s
   end
 end
